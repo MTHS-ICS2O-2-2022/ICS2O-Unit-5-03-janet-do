94256d4da1ef9b30887a822da2e17afba87caba9
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-01 18:07:03 +0000

    sync

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Janet Do All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Janet Do
 // Created on: Sep 2020
-// This file contains the JS functions for index.html
+// This file checks the user's age to see what movies they can see
+
+"use strict"
+
+function calculate() {
+// input
+const age = prompt('Enter your age:')
+
+// process and output
+if (age >= 18) {
+  console.log('You can see an R rated movie by yourself')
+} else if (age >= 13) {
+  console.log('You can see a PG movie by youself')
+} else if (age >= 5) {
+  console.log('You can see a G or PG movie by yourself' )
+} else {
+   console.log('You are too young for anything')
+}
+
+console.log('\nDone.') 
