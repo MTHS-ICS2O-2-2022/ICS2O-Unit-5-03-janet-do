bb17edef564be60caf15fd7148db13c4c31e8b03
janet-do <124389477+janet-do@users.noreply.github.com>
2023-05-02 17:21:55 +0000

    sync

--- go_app/main.go
+++ go_app/main.go
@@ -6,25 +6,25 @@
 package main
 
 import (
-    "fmt"
-    "strconv"
+	"fmt"
+	"strconv"
 )
 
 func main() {
-    var ageInput string
-    fmt.Print("Enter your age: ")
-    fmt.Scanln(&ageInput)
+	var ageInput string
+	fmt.Print("Enter your age: ")
+	fmt.Scanln(&ageInput)
 
-    age, err := strconv.ParseFloat(ageInput, 64)
-    if err != nil {
-        fmt.Println("Invalid input, please enter a number.")
-    } else if age >= 17 {
-        fmt.Println("You can see an R rated movie alone.")
-    } else if age >= 13 {
-        fmt.Println("You can see a PG-13 movie alone.")
-    } else if age >= 6 {
-        fmt.Println("You can see a G or PG movie alone.")
-    } else {
-        fmt.Println("You're too young to see anything.")
-    }
+	age, err := strconv.ParseFloat(ageInput, 64)
+	if err != nil {
+		fmt.Println("Invalid input, please enter a number.")
+	} else if age >= 17 {
+		fmt.Println("You can see an R rated movie alone.")
+	} else if age >= 13 {
+		fmt.Println("You can see a PG-13 movie alone.")
+	} else if age >= 6 {
+		fmt.Println("You can see a G or PG movie alone.")
+	} else {
+		fmt.Println("You're too young to see anything.")
+	}
 }
